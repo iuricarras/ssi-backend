0acd41887dccb262825ab290f1acf667c455cb70
Iuri Carrasqueiro <iuri.carrasqueiro@gmail.com>
2025-11-20 15:39:43 +0000

    FIX: Alterado parametros da função

--- app/api/message/hmac.py
+++ app/api/message/hmac.py
@@ -1,9 +1,10 @@
 import jwt
 import hashlib
+
 ## Utilizar esta função para gerar a assinatura HMAC de uma mensagem
 ## Utilizar no retorno de todos os endpoints
-def generate_hmac_signature(message: str, userJWT: str, userNounce: str) -> str:
-    secret = f"{userJWT}.{userNounce}"
+def generate_hmac_signature(message: str, userID: str, userNounce: str) -> str:
+    secret = f"{userID}.{userNounce}"
     h = hashlib.new('sha256')
     h.update(secret.encode('utf-8'))
     h.hexdigest()
